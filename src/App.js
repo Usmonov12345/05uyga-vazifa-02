06ee7302e06522e85878d5a7de247fd228716258
Usmonov12345 <toxirusmonov04@gmail.com>
2023-02-12 02:01:14 +0500

    uyga vazifa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./componet/Header";
+import Main from "./componet/Main";
+import "./Main.css"
+function App() {
+  return (
+    <div className="App">
+      < Header/>
+      < Main/>
+    </div>
+  );
+}
+
+export default App;
